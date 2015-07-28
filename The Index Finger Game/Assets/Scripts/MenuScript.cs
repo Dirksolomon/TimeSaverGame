94b85b891371380eb054de586b0bc385bbbd3fe9
masterTimMi <masterTimMi@gmail.com>
2015-07-28 12:54:46 +0300

    Update
    Several fixes

--- The Index Finger Game/Assets/Scripts/MenuScript.cs
+++ The Index Finger Game/Assets/Scripts/MenuScript.cs
@@ -1,24 +1,20 @@
-﻿using UnityEngine;
+﻿/*
+	Start menu script.
+	Updated 2015-7-28 12:19 by Mikko
+*/
+using UnityEngine;
 using System.Collections;
 
-public class MenuScript : MonoBehaviour {
+public class MenuScript:MonoBehaviour{
+	public void StartGame()	{
+		Application.LoadLevel("1_Game"); // new game
+	}
+	public void QuitGame(){
+		Application.Quit();
+	}
 
-	public void StartGame()
-	{
-		Application.LoadLevel ("1_Game");
-		Time.timeScale = 1;
-	}
-	public void QuitGame()
-	{
-		Application.Quit ();
-	}
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	void Start(){}
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update(){}
 }
