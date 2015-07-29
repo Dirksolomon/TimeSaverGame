325d3d904a0398e4d04e3e607fbdcb7c880a255b
masterTimMi <masterTimMi@gmail.com>
2015-07-29 11:41:16 +0300

    Menu stuff finished.

--- The Index Finger Game/Assets/Scripts/MenuScript.cs
+++ The Index Finger Game/Assets/Scripts/MenuScript.cs
@@ -11,6 +11,7 @@ public class MenuScript:MonoBehaviour{
 	public GameObject pnlAbout;
 	public GameObject pnlExit;
 
+	public bool continuable=true;
 	public bool aboutShown=false;
 	public bool exitShown=false;
 
@@ -26,37 +27,67 @@ public class MenuScript:MonoBehaviour{
 
 	// About the game
 	public void ShowAboutGame(){
+		aboutShown = true;
 		pnlAbout.SetActive(true);
 	}
 	public void HideAboutGame(){
+		aboutShown = false;
 		pnlAbout.SetActive(false);
 	}
 
 	// Exit the game
 	public void ShowExitGame(){
+		print ("Please, do not go away!");
+		exitShown = true;
 		pnlExit.SetActive(true);
 	}
 	public void HideExitGame(){
+		print ("Oh yeah, keep playing...");
+		exitShown = false;
 		pnlExit.SetActive(false);
 	}
 
 	// Quitting the game
 	public void QuitGame(){
+		print ("Bye.");
 		Application.Quit();
 	}
 
 	// Use this for initialization
 	void Start(){
-		if (!PlayerPrefs.HasKey ("CheckPoint"))
-			btnContinue.SetActive(false);
+		if (!PlayerPrefs.HasKey ("CheckPoint")) {
+			continuable=false;
+			btnContinue.SetActive (false);
+		}
 	}
 	// Update is called once per frame
 	void Update(){
-		if(aboutShown)
-			if(Input.GetKey(KeyCode.Escape))
-			   HideAboutGame();
-	   if(exitShown)
-	   		if(Input.GetKey(KeyCode.Escape))
-			   HideExitGame();
+
+		// Escape key closes the window if it is open
+		if (aboutShown) {
+			if (Input.GetKeyDown (KeyCode.Escape))
+				HideAboutGame ();
+		}
+		else if(exitShown){
+			if (Input.GetKeyDown (KeyCode.Escape))
+				HideExitGame ();
+			else if (Input.GetKeyDown (KeyCode.Return))
+				QuitGame ();
+		}
+
+		else {
+			// Escape key shows exit window if there is no more open windows
+			if (Input.GetKeyDown (KeyCode.Escape))
+				ShowExitGame ();
+
+			// Enter acts as a press on the first menu item
+			else if (Input.GetKeyDown (KeyCode.Return)){
+				// Continue game if possible
+				if(continuable)
+					ContinueGame();
+				// Else start new game
+				else StartNewGame();
+			}
+		}
 	}
 }
