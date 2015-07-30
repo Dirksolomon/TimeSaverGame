596f30ecb1afabcca5741c35a710ad9ce8ef8603
unknown <ojuho001@MDMA325W03.ksamk.ad>
2015-07-30 12:01:41 +0300

    Prefabs back, elevator somewhat working.

--- The Index Finger Game/Assets/Scripts/Enemy.cs
+++ The Index Finger Game/Assets/Scripts/Enemy.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
-
+	public Transform upLook;
+	public Transform straightLook;
+	public Transform downLook;
 	public GameObject enemyGuard;
 
 	// Use this for initialization
@@ -12,7 +14,12 @@ public class Enemy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		enemyGuard.transform.Translate(Vector2.right*2);
-		enemyGuard.transform.Translate(-Vector2.right*2);
+		//enemyGuard.transform.Translate(Vector2.right*2);
+		//enemyGuard.transform.Translate(-Vector2.right*2);
+		Debug.DrawRay(transform.position, upLook.transform * 0.4f, Color.red);
+		if(Physics2D.Raycast(transform.position, upLook.transform, 0.4f))
+		{
+			Debug.Log ("Player in sight!");
+		}
 	}
 }
