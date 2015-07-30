119844ae748afd52287992c5f31ac85c7869e44f
Kerbow <PowPow@hulabaloo.com>
2015-07-30 04:10:58 +0300

    PlayerMove: Changed jumping so you do not jump if you keep the button down. Made NonClingy physic2d material that makes player slide down on walls if they are not meant to cling on them. ButtonScript: Made buttonscript that opens a door on Level 1 which I edited a bit for testing purposes

---
+++ The Index Finger Game/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+	public GameObject door;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (Input.GetKeyDown("up")) 
+		{
+			door.SetActive(false);
+			Debug.Log ("Works!");
+		}
+	}
+	
+}
