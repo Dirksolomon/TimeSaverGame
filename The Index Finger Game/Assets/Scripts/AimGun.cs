74cf51d1327d2d6eac0a47a7237de086dff46f5a
unknown <ojuho001@MDMA325W03.ksamk.ad>
2015-07-31 10:12:59 +0300

    Started on gun script, made small changes to basement and enemies.

---
+++ The Index Finger Game/Assets/Scripts/AimGun.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AimGun : MonoBehaviour {
+	public Transform Player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector2 LookAtPlayer = (Player.transform.position - transform.position).normalized;
+		float rotateZ = Mathf.Atan2 (LookAtPlayer.y, LookAtPlayer.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler (0f, 0f, rotateZ + 90);
+	}
+}
